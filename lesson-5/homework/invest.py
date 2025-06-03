4e8035652e8bcb00cdde511f3e868723665e468d
Muxammadjon123 <m.uralov@newuu.uz>
2025-06-03 15:17:07 +0500

    Update invest.py

--- lesson-5/homework/invest.py
+++ lesson-5/homework/invest.py
@@ -1,7 +1,7 @@
 def invest(amount,year,rate):
     for i in range(1,year+1):
         amount=amount+amount*rate
-        print(f"year {year}:{round(amount,2)}")
+        print(f"year {i}:{round(amount,2)}")
 
 invest(100,4,0.05)
 
